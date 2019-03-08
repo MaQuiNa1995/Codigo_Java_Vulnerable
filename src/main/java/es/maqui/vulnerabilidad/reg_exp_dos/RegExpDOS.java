c8784f89482fa3e523725fbb50b4ee2e5f1ae88c
MaQuiNa1995 <maquina1995@gmail.com>
2019-03-08 18:04:42 +0100

    formateo javadoc y renombrado de una clase

--- src/main/java/es/maqui/vulnerabilidad/reg_exp_dos/RegExpDOS.java
+++ src/main/java/es/maqui/vulnerabilidad/reg_exp_dos/RegExpDOS.java
@@ -6,8 +6,13 @@ package es.maqui.vulnerabilidad.reg_exp_dos;
  * @author MaQuiNa1995
  *
  */
-public class RegExpDOS {
+public class RegExpDos {
 
+	/**
+	 * La vulnerabilidad consiste en el uso de patrones no seguros en el que con
+	 * cierta cadena pueden llegar a dar un tiempo de respuesta muy alto petando en
+	 * el camino
+	 */
 	public static void main(String... args) {
 		// Patron usado
 		String patron = "(aa|aab?)*";
